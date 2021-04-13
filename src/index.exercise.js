9dedb1289b73bdc86237762600ba1b77e4f5c51c
Renny <renny@bunnyhug.net>
2021-04-12 19:54:09 -0600

    Use @reach/dialog

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,19 +1,33 @@
+import "@reach/dialog/styles.css"
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
 
 function App() {
+    const [openModel, setOpenModel] = React.useState('none')
     return <div>
         <Logo width="80" height="80"/>
         <h1>Bookshelf</h1>
         <div>
-            <button onClick={() => alert('login')}>Login</button>
+            <button onClick={() => setOpenModel('login')}>Login</button>
         </div>
         <div>
-            <button onClick={() => alert('register')}>Register</button>
+            <button onClick={() => setOpenModel('register')}>Register</button>
         </div>
+        <Dialog aria-label="Login form" isOpen={openModel === 'login'}>
+            <div>
+                <button onClick={() => setOpenModel('none')}>Close</button>
+            </div>
+            <h3>Login</h3>
+        </Dialog>
+        <Dialog aria-label="Register form" isOpen={openModel === 'register'}>
+            <div>
+                <button onClick={() => setOpenModel('none')}>Close</button>
+            </div>
+            <h3>Register</h3>
+        </Dialog>
     </div>
 }
 
-ReactDOM.render(App(), document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
