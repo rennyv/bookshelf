08b8df773cb42a8968ec1d550d55c0a72a6be914
Renny <renny@bunnyhug.net>
2021-04-12 20:54:30 -0600

    Styled with Emotion CSS prop

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,10 +1,9 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
 import 'bootstrap/dist/css/bootstrap-reboot.css'
 import '@reach/dialog/styles.css'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-// 🐨 you'll need to import some new components that you'll be creating
-// in this file
-
 import {Button, Input, FormGroup} from './components/lib'
 import {Modal, ModalContents, ModalOpenButton} from './components/modal'
 import {Logo} from './components/logo'
@@ -22,16 +21,19 @@ function LoginForm({onSubmit, submitButton}) {
 
   // 🐨 this <form> could use a css prop
   // 🎨
-  //    display: 'flex',
-  //    flexDirection: 'column',
-  //    alignItems: 'stretch',
-  //    '> div': {
-  //      margin: '10px auto',
-  //      width: '100%',
-  //      maxWidth: '300px',
+
   //    },
   return (
-    <form onSubmit={handleSubmit}>
+    <form css={{
+     display: 'flex',
+     flexDirection: 'column',
+     alignItems: 'stretch',
+     '> div': {
+       margin: '10px auto',
+       width: '100%',
+       maxWidth: '300px',
+     },
+    }} onSubmit={handleSubmit}>
       <FormGroup>
         <label htmlFor="username">Username</label>
         <Input id="username" />
@@ -54,27 +56,22 @@ function App() {
     console.log('register', formData)
   }
 
-  // 🐨 this div could use a css prop to get its children rendered nicer
-  // 🎨
-  //    display: 'flex',
-  //    flexDirection: 'column',
-  //    alignItems: 'center',
-  //    justifyContent: 'center',
-  //    width: '100%',
-  //    height: '100vh',
   return (
-    <div>
+    <div css={{
+     display: 'flex',
+     flexDirection: 'column',
+     alignItems: 'center',
+     justifyContent: 'center',
+     width: '100%',
+     height: '100vh',
+    }}>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      {/*
-        🐨 the two buttons are too close, let's space them out
-          🎨 apply this to the div right below
+      <div css={{
             display: 'grid',
             gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
             gridGap: '0.75rem',
-      */}
-      {/* 🐨 And make sure to use the new Button component for all these buttons */}
-      <div>
+      }}>
         <Modal>
           <ModalOpenButton>
             <Button variant="primary">Login</Button>
