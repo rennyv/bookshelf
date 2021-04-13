23cad44f768a9c46822e519b44bc6f1e4cbea293
Renny <renny@bunnyhug.net>
2021-04-12 20:04:25 -0600

    Created a loginForm component

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -4,8 +4,43 @@ import ReactDOM from 'react-dom'
 import {Dialog} from '@reach/dialog'
 import {Logo} from './components/logo'
 
+function LoginForm({onSubmit, buttonText}) {
+    function handleSubmit(event){
+        event.preventDefault()
+
+        const {username, password} = event.target.elements
+
+        onSubmit({
+            username: username.value,
+            password: password.value
+        })
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input id="password" type="password" />
+            </div>
+            <button type="submit">{buttonText}</button>
+        </form>
+    )
+}
+
 function App() {
     const [openModel, setOpenModel] = React.useState('none')
+
+    function login(formData){
+        console.log('login', formData)
+    }
+
+    function register(formData){
+        console.log('register', formData)
+    }
+
     return <div>
         <Logo width="80" height="80"/>
         <h1>Bookshelf</h1>
@@ -20,12 +55,14 @@ function App() {
                 <button onClick={() => setOpenModel('none')}>Close</button>
             </div>
             <h3>Login</h3>
+            <LoginForm onSubmit={login} buttonText="Login"/>
         </Dialog>
-        <Dialog aria-label="Register form" isOpen={openModel === 'register'}>
+        <Dialog aria-label="Registation form" isOpen={openModel === 'register'}>
             <div>
                 <button onClick={() => setOpenModel('none')}>Close</button>
             </div>
             <h3>Register</h3>
+            <LoginForm onSubmit={register} buttonText="Register"/>
         </Dialog>
     </div>
 }
