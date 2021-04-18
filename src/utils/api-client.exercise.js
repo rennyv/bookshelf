bb823247715c50c4382789810624218c81b54d49
Renny <renny@bunnyhug.net>
2021-04-18 15:37:29 -0600

    Automatically logging out on 401

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,3 +1,4 @@
+import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
 function client(endpoint, {token, headers: customHeaders, ...customConfig} = {}) {
@@ -11,6 +12,11 @@ function client(endpoint, {token, headers: customHeaders, ...customConfig} = {})
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401){
+      await auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please log in'})
+    }
     const data = await response.json()
     if (response.ok) {
       return data
