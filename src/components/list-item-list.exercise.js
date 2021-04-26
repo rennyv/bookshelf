e87bcfac1803bcb38462c58c91b01b2eb93adb8a
Renny <renny@bunnyhug.net>
2021-04-25 21:03:06 -0600

    Created a useListItem(s) Custom Hook

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-import {useQuery} from 'react-query'
-import {client} from 'utils/api-client'
+import {useListItems} from 'utils/list-items'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
 
@@ -11,12 +10,8 @@ function ListItemList({
   noListItems,
   noFilteredListItems,
 }) {
-  const {data: listItems} = useQuery({
-    queryKey: 'list-items',
-    queryFn: () => client('list-items', {token: user.token}).then(data => data.listItems)
-  })
-
-  const filteredListItems = listItems?.filter(filterListItems)
+  const listItems = useListItems(user)
+  const filteredListItems =  listItems?.filter(filterListItems)
 
   if (!listItems?.length) {
     return <div css={{marginTop: '1em', fontSize: '1.2em'}}>{noListItems}</div>
--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -1,12 +1,20 @@
-/*
 import {useQuery} from 'react-query'
-import {client} from 'api-client'
+import {client} from './api-client'
  
-function useList(user){
-    const [data: result] = useQuery({
+function useListItems(user){
+    const {data: listItems} = useQuery({
         queryKey: 'list-items',
         queryFn: () => client('list-items', {token: user.token}).then(data => data.listItems)
       })
-    
-    return result : null
-}*/
+
+    return listItems ?? []
+}
+
+function useListItem(user, bookId) {
+    const listItems = useListItems(user)
+    const listItem = listItems.find(li => li.bookId === bookId) ?? null
+
+    return listItem
+}
+
+export {useListItem, useListItems}
