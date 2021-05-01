2babde618e3c63e4c34526b3dab84c08f29bb15f
Renny <renny@bunnyhug.net>
2021-04-30 20:52:38 -0600

    Created ModalContentsBase

--- src/components/modal.exercise.js
+++ src/components/modal.exercise.js
@@ -2,7 +2,9 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-import {Dialog} from './lib'
+import {Dialog,CircleButton,Button} from 'components/lib'
+import VisuallyHidden from '@reach/visually-hidden'
+
 
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
 
@@ -28,11 +30,26 @@ function ModalOpenButton({children: child}) {
   })
 }
 
-function ModalContents(props) {
+function ModalContentsBase(props) {
   const [isOpen, setIsOpen] = React.useContext(ModalContext)
   return (
     <Dialog isOpen={isOpen} onDismiss={() => setIsOpen(false)} {...props} />
   )
 }
 
-export {Modal, ModalDismissButton, ModalOpenButton, ModalContents}
+function ModalContents({title, children, ...props}) {
+  return  <ModalContentsBase {...props}>
+            <div css={{display: 'flex', justifyContent: 'flex-end'}}>
+              <ModalDismissButton>
+                <CircleButton>
+                  <VisuallyHidden>Close</VisuallyHidden>
+                  <span aria-hidden>×</span>
+                </CircleButton>
+              </ModalDismissButton>
+            </div>
+            <h3 css={{textAlign: 'center', fontSize: '2em'}}>{title}</h3>
+            {children}
+          </ModalContentsBase>
+}
+
+export {Modal, ModalDismissButton, ModalOpenButton, ModalContents, ModalContentsBase}
--- src/unauthenticated-app.exercise.js
+++ src/unauthenticated-app.exercise.js
@@ -2,10 +2,8 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-import VisuallyHidden from '@reach/visually-hidden'
 import {
   Input,
-  CircleButton,
   Button,
   Spinner,
   FormGroup,
@@ -13,7 +11,6 @@ import {
 } from './components/lib'
 import {
   Modal,
-  ModalDismissButton,
   ModalContents,
   ModalOpenButton,
 } from './components/modal'
@@ -73,17 +70,6 @@ function LoginForm({onSubmit, submitButton}) {
   )
 }
 
-const circleDismissButton = (
-  <div css={{display: 'flex', justifyContent: 'flex-end'}}>
-    <ModalDismissButton>
-      <CircleButton>
-        <VisuallyHidden>Close</VisuallyHidden>
-        <span aria-hidden>×</span>
-      </CircleButton>
-    </ModalDismissButton>
-  </div>
-)
-
 function UnauthenticatedApp() {
   const {login, register} = useAuth()
   return (
@@ -110,9 +96,7 @@ function UnauthenticatedApp() {
           <ModalOpenButton>
             <Button variant="primary">Login</Button>
           </ModalOpenButton>
-          <ModalContents aria-label="Login form">
-            {circleDismissButton}
-            <h3 css={{textAlign: 'center', fontSize: '2em'}}>Login</h3>
+          <ModalContents aria-label="Login form" title="Login">
             <LoginForm onSubmit={login} submitButton={<Button variant="primary">Login</Button>} />
           </ModalContents>
         </Modal>
@@ -120,9 +104,7 @@ function UnauthenticatedApp() {
           <ModalOpenButton>
             <Button variant="secondary">Register</Button>
           </ModalOpenButton>
-          <ModalContents aria-label="Register form">
-            {circleDismissButton}
-            <h3 css={{textAlign: 'center', fontSize: '2em'}}>Register</h3>
+          <ModalContents aria-label="Register form" title="Register">
             <LoginForm onSubmit={register} submitButton={<Button variant="secondary">Register</Button>} />
           </ModalContents>
         </Modal>
