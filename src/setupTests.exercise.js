7c3d7b35ac44ab682ecc4d26623c3d38b0d54b29
Renny <renny@bunnyhug.net>
2021-05-02 10:19:04 -0600

    Moved server setup to setupTests.js

--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -1,1 +1,5 @@
-// this isn't used in the soultion. Only in the extra credit
+import {server} from 'test/server'
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -8,10 +8,6 @@ const apiURL = process.env.REACT_APP_API_URL
 jest.mock('react-query')
 jest.mock('auth-provider')
 
-beforeAll(() => server.listen())
-afterAll(() => server.close())
-afterEach(() => server.resetHandlers())
-
 test('calls fetch at the endpoint with the arguments for GET requests', async () => {
   const endpoint = 'test-endpoint'
   const mockResult ={ mockValue: 'VALUE'  }
